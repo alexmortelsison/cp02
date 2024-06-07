3ae28e3c34e79e4a1244632c272e7005eb6a90bc
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-06-07 12:38:14 +0900

    updated UI

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cp02/services/firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,8 +10,100 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  FirestoreService firestoreService = FirestoreService();
+  final textController = TextEditingController();
+
+  void openNoteBox(String? docID) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          content: TextField(
+            controller: textController,
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                if (docID == null) {
+                  firestoreService.addNote(textController.text);
+                } else {
+                  firestoreService.updateNote(docID, textController.text);
+                }
+                textController.clear();
+                Navigator.pop(context);
+              },
+              child: Text(
+                'Add',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.tertiary,
+                ),
+              ),
+            )
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: const Center(
+          child: Text('C R U D'),
+        ),
+      ),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => openNoteBox(null),
+        child: Icon(
+          Icons.add,
+          color: Theme.of(context).colorScheme.tertiary,
+        ),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: firestoreService.getNotesStream(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            List notesList = snapshot.data!.docs;
+            return ListView.builder(
+              itemCount: notesList.length,
+              itemBuilder: (context, index) {
+                DocumentSnapshot document = notesList[index];
+                String docId = document.id;
+                Map<String, dynamic> data =
+                    document.data() as Map<String, dynamic>;
+                String noteText = data['note'];
+                return ListTile(
+                  title: Text(noteText),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(
+                        onPressed: () => openNoteBox(docId),
+                        icon: const Icon(
+                          Icons.settings,
+                        ),
+                      ),
+                      IconButton(
+                        onPressed: () => firestoreService.deleteNote(docId),
+                        icon: const Icon(
+                          Icons.delete,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
+          } else {
+            return const Center(
+              child: Text('No Notes'),
+            );
+          }
+        },
+      ),
+    );
   }
 }
