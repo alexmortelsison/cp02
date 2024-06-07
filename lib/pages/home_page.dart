899d5e764a8e6dd36354a635f0f7566ac87cf5d0
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2024-06-08 06:01:55 +0900

    fixed bug

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cp02/services/firestore.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -51,7 +49,9 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(
-        title: const Center(child: Text('C R U D')),
+        title: const Center(
+          child: Text('C R U D'),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => openNoteBox(null),
@@ -60,47 +60,50 @@ class _HomePageState extends State<HomePage> {
           color: Theme.of(context).colorScheme.tertiary,
         ),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: firestoreService.getNotesStream(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            List notesList = snapshot.data!.docs;
-            return ListView.builder(
-              itemCount: notesList.length,
-              itemBuilder: (context, index) {
-                DocumentSnapshot document = notesList[index];
-                String docID = document.id;
-                Map<String, dynamic> data =
-                    document.data() as Map<String, dynamic>;
-                String noteText = data['note'];
-                return ListTile(
-                  title: Text(noteText),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                        onPressed: () => openNoteBox(docID),
-                        icon: const Icon(
-                          Icons.settings,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 25),
+        child: StreamBuilder<QuerySnapshot>(
+          stream: firestoreService.getNotesStream(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              List notesList = snapshot.data!.docs;
+              return ListView.builder(
+                itemCount: notesList.length,
+                itemBuilder: (context, index) {
+                  DocumentSnapshot document = notesList[index];
+                  String docID = document.id;
+                  Map<String, dynamic> data =
+                      document.data() as Map<String, dynamic>;
+                  String noteText = data['note'];
+                  return ListTile(
+                    title: Text(noteText),
+                    trailing: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                          onPressed: () => openNoteBox(docID),
+                          icon: const Icon(
+                            Icons.edit_note_outlined,
+                          ),
                         ),
-                      ),
-                      IconButton(
-                        onPressed: () => firestoreService.deleteNote(docID),
-                        icon: const Icon(
-                          Icons.delete,
+                        IconButton(
+                          onPressed: () => firestoreService.deleteNote(docID),
+                          icon: const Icon(
+                            Icons.delete_outline,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            );
-          } else {
-            return const Center(
-              child: Text('No notes...'),
-            );
-          }
-        },
+                      ],
+                    ),
+                  );
+                },
+              );
+            } else {
+              return const Center(
+                child: Text('No notes...'),
+              );
+            }
+          },
+        ),
       ),
     );
   }
